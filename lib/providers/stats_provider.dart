38d0608697264769e0f69fddad82983142a8e531
Michael Speed <michaelcspeed@gmail.com>
2024-10-26 16:51:09 +0200

    End screen update

--- lib/providers/stats_provider.dart
+++ lib/providers/stats_provider.dart
@@ -3,18 +3,39 @@ import 'package:medito/constants/strings/string_constants.dart';
 import 'package:medito/models/local_all_stats.dart';
 import 'package:medito/utils/stats_manager.dart';
 
-final statsProvider = FutureProvider<LocalAllStats>((ref) async {
-  var statsManager = StatsManager();
+final statsManagerProvider = Provider<StatsManager>((ref) => StatsManager());
 
-  try {
-    await statsManager.initialize();
-    return await statsManager.localAllStats;
-  } catch (e, stackTrace) {
-    if (e is StateError) {
-      return Future.error(
-          'Failed to initialize StatsManager: ${e.message}', stackTrace);
-    }
-
-    return Future.error(StringConstants.statsLoadError, stackTrace);
-  }
+final statsProvider = AsyncNotifierProvider<StatsNotifier, LocalAllStats>(() {
+  return StatsNotifier();
 });
+
+class StatsNotifier extends AsyncNotifier<LocalAllStats> {
+  @override
+  Future<LocalAllStats> build() async {
+    return _fetchStats();
+  }
+
+  Future<LocalAllStats> _fetchStats() async {
+    var statsManager = ref.read(statsManagerProvider);
+
+    try {
+      await statsManager.initialize();
+
+      return await statsManager.localAllStats;
+    } catch (e, stackTrace) {
+      if (e is StateError) {
+        throw AsyncError(
+          'Failed to initialize StatsManager: ${e.message}',
+          stackTrace,
+        );
+      }
+
+      throw AsyncError(StringConstants.statsLoadError, stackTrace);
+    }
+  }
+
+  Future<void> refresh() async {
+    state = const AsyncValue.loading();
+    state = await AsyncValue.guard(() => _fetchStats());
+  }
+}
